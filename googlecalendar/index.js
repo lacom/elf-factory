89811af10938f609ba6633e5d04f3922b5cb25a0
Carl Mueller <cmuell89@gmail.com>
2015-07-31 21:58:58 -0700

    On branch clm  Your branch is up-to-date with 'origin/clm'.
     Changes to be committed:
    	modified:   bin/elf
    	new file:   bin/elf-open
    	new file:   googlecalendar/.env
    	new file:   googlecalendar/index.js
    	new file:   googlecalendar/package.json
    	new file:   templates/.env

---
+++ googlecalendar/index.js
@@ -0,0 +1,9 @@
+// Load any config vars from .env file
+require('dotenv').load();
+
+exports.handler = function(event, context) {
+	// event.input [string] will contain any text submitted along with your command
+	// event.client [string] will contain the client the command was submitted from, either "slack" or "sms"
+	// event.value [string] will contain any value we are able to parse from event.input
+  context.succeed('YOUR_EPIC_RESPONSE_GOES_HERE');
+};
